63288d00458df9f28f13e64a56e7ecf982bbbe20
y.dubovitsky <y.dubovitsky@gmail.con>
2020-10-10 12:58:51 +0300

    Added a new template to the project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../assets/css/material-dashboard.css']
 })
 export class AppComponent {
   title = 'angular-to-do-list';
